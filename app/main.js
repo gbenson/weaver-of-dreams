5f13020f57c80c2a2f80f8e6aa9c02c27062001d
Gary Benson <gary@gbenson.net>
2022-11-23 21:17:56 +0000

    Start implementing class Draft

--- app/main.js
+++ app/main.js
@@ -1,3 +1,21 @@
+class Draft {
+  constructor({num_shafts,
+	       num_treadles,
+	       rising_shed,
+	       tieup,
+	       warp,
+	       weft,
+	       //liftplan,
+    //date, title, author, address, email, telephone, fax, notes])
+	      }) {
+    this.numShafts = num_shafts
+    this.numTreadles = num_treadles
+    this.isRisingShed = rising_shed
+    this.warp = warp //XXX just so we can .length it below
+    this.weft = weft //XXX ditto
+  }
+}
+
 const shaftsEl = document.querySelector("input[name=shafts]")
 const treadlesEl = document.querySelector("input[name=treadles]")
 const threadsPUEl = document.querySelector("input[name=threadspu]")
@@ -5,26 +23,20 @@ const threadPUsEl = document.querySelector("input[name=threadpus]")
 const treadsPUEl = document.querySelector("input[name=treadspu]")
 const treadPUsEl = document.querySelector("input[name=treadpus]")
 
-function setDefaults() {
-  if (shaftsEl.value != "")
-    return;
-  if (treadlesEl.value != "")
-    return;
-  if (threadsPUEl.value != "")
-    return;
-  if (threadPUsEl.value != "")
-    return;
-  if (treadsPUEl.value != "")
-    return;
-  if (treadPUsEl.value != "")
-    return;
+async function populate() {
 
-  shaftsEl.value = 4
-  treadlesEl.value = shaftsEl.value
-  threadsPUEl.value = 18
-  threadPUsEl.value = 2
-  treadsPUEl.value = threadsPUEl.value
-  treadPUsEl.value = threadPUsEl.value
+  const requestURL = "../test/79703.json"
+  const request = new Request(requestURL)
+  const response = await fetch(request)
+
+  const draft = new Draft(await response.json())
+
+  shaftsEl.value = draft.numShafts
+  treadlesEl.value = draft.numTreadles
+  threadsPUEl.value = draft.warp.length //XXX spot repeats
+  treadsPUEl.value = draft.weft.length //XXX ditto
+  threadPUsEl.value = Number(draft.isRisingShed) //XXX make a checkbox
+  treadPUsEl.value = 2301023 //XXX i.e. success!
 }
 
-setDefaults()
+populate()
